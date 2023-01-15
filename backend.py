fceec3f1b0d4ff7b787fabd5516f7c09482834d3
Mikhail Bobrov <BOBRTVLOL@gmail.com>
2023-01-15 19:57:54 +0100

    Course day 33. Finished. "Student project" was to fix temperature output from Kelvin to Celsius and make a try except for nonexistent place prompt.

--- backend.py
+++ backend.py
@@ -4,20 +4,17 @@ import requests
 
 def get_data(place, forecast_days=1):
     key = os.getenv("OPENWEATHER_API_KEY")
-    url = f"https://pro.openweathermap.org/data/2.5/forecast/hourly?q={place}&appid={key}"
-    print(url)
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={key}"
 
     response = requests.get(url)
     data = response.json()
 
-    filtered_data = data["list"]
-    number_of_values = forecast_days * 24
-    filtered_data = filtered_data[:number_of_values]
-
-    if type_of_info == "Temperature":
-
-    elif type_of_info == "Sky":
-
+    try:
+        filtered_data = data["list"]
+        number_of_values = forecast_days * 8
+        filtered_data = filtered_data[:number_of_values]
+    except KeyError:
+        filtered_data = "ERROR"
 
     return filtered_data
 
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ st.title("Weather Forecast for the Next Days")
 
 place = st.text_input("Place", value="Marseille")
 
-number_of_days = st.slider("Number of days", 1, 4)
+number_of_days = st.slider("Number of days", 1, 5)
 
 selection = st.selectbox("Data", ("Temperature", "Sky"))
 
@@ -16,11 +16,22 @@ st.header(f"{selection} for the next {number_of_days} days in {place}")
 if place:
     data = get_data(place, number_of_days)
 
+    if data == "ERROR":
+        selection = ""
+        st.info("That place is not registered or does not exist!")
+
     if selection == "Temperature":
-        data = [dict["main"]["temp"] for dict in data]
-        figure = px.line(x=dates, y=data, labels={"x": "date", "y": "temperature"})
+        temperatures = [dict["main"]["temp"]-273.15 for dict in data]
+        dates = [dict["dt_txt"] for dict in data]
+        figure = px.line(x=dates, y=temperatures, labels={"x": "date", "y": "temperature"})
         st.plotly_chart(figure)
 
     if selection == "Sky":
         data = [dict["weather"][0]["main"] for dict in data]
-        st.image()
+        image_paths = {"Clear": "images/clear.png",
+                       "Clouds": "images/clouds.png",
+                       "Rain": "images/rain.png",
+                       "Snow": "images/snow.png"}
+        images = [image_paths[condition] for condition in data]
+        print(data)
+        st.image(images, width=150)
